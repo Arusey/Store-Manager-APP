360ef12cda953f66bb4a48245a11c108e1f0a7ef
Arusey <kevinkibetw7@gmail.com>
2018-10-18 22:48:56 +0300

    [Feature #161309671] Add login endpoint for tests to pass

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, Blueprint
 from flask_restful import Api, Resource
-from .views import SignUp
+from .views import SignUp, Login
 myblue = Blueprint("api", __name__, url_prefix="/storemanager/api/v1")
 
 api = Api(myblue)
 api.add_resource(SignUp, '/auth/signup')
-# api.add_resource(Login, '/auth/login')
+api.add_resource(Login, '/auth/login')
